ef626e5e71bd814c0fa0ef8281c1ee82c8646663
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2022-04-20 16:07:53 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -68,18 +68,13 @@ class Main extends Sprite
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
-
-		#if !debug
-		initialState = TitleState;
-		#end
+		
+		SUtil.doTheCheck();
 	
 		ClientPrefs.loadDefaultKeys();
 		// fuck you, persistent caching stays ON during sex
 		FlxGraphic.defaultPersist = true;		
 		// the reason for this is we're going to be handling our own cache smartly
-		
-                SUtil.doTheCheck();
-		
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
