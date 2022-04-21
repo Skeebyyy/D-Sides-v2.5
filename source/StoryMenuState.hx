f56f399d72aee4b0b26d0a7564478d9bd0aed442
Skeebyyy <95153655+Skeebyyy@users.noreply.github.com>
2022-04-20 21:16:22 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -181,7 +181,7 @@ class StoryMenuState extends MusicBeatState
 		changeWeek();
 		
 	        #if android
-	        addVirtualPad(UP_DOWN, A_B);
+	        addVirtualPad(FULL, A_B);
                 #end
 
 		super.create();
